5000d64d92761b19b2cd57e8d80c6384749f3f14
TonimatasDEV <amatascubero@gmail.com>
2022-12-11 19:34:41 +0100

    Fix sound action

--- src/main/java/net/tonimatasmc/playerdata/recompense/actions/Sound.java
+++ src/main/java/net/tonimatasmc/playerdata/recompense/actions/Sound.java
@@ -1,14 +1,9 @@
 package net.tonimatasmc.playerdata.recompense.actions;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public class Sound {
     public static void execute(String sound, Player player) {
-        String x = String.valueOf(player.getLocation().getBlockX());
-        String y = String.valueOf(player.getLocation().getBlockY());
-        String z = String.valueOf(player.getLocation().getBlockZ());
-
-        Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), "playsound " + sound + " master " + player.getName() + " " + x + " " + y + " " + z + " ");
+        player.playSound(player.getLocation(), sound, 10f, 10f);
     }
 }
